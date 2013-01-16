8728a81432351063d7c23f391c986aabeb520d0b
Xavier <Xavier@172.16.1.50>
2013-01-16 09:45:56 +0100

    Add a basic display class (GUI)

---
+++ src/theisland/gui/Gui.java
@@ -0,0 +1,13 @@
+package theisland.gui;
+
+/**
+ *
+ * @author Xavier
+ */
+public class Gui {
+    public void display(String displayString) {
+        if (!displayString.isEmpty()) {
+            System.out.println(displayString);
+        }
+    }
+}
