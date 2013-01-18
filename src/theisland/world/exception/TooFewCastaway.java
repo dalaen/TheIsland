3dc0a21352f61f2bf994c66461ff1d1d4e59a967
Xavier Poirot <xavier.poirot@dalaen.com>
2013-01-18 09:53:20 +0100

    Add exception handling for castaway primary setting

---
+++ src/theisland/world/exception/TooFewCastaway.java
@@ -0,0 +1,10 @@
+package theisland.world.exception;
+
+public class TooFewCastaway extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/theisland/world/exception/TooManyCastaway.java
@@ -0,0 +1,11 @@
+package theisland.world.exception;
+
+public class TooManyCastaway extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2208837495591813817L;
+
+	public TooManyCastaway() {
+	}
+}
