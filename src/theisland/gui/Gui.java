a0f30fcc2be0d6d3736b74060f91b085ecaa3fb2
Xavier Poirot <xavier.poirot@dalaen.com>
2013-01-18 09:54:13 +0100

    Add various essential functions

--- src/theisland/gui/Gui.java
+++ src/theisland/gui/Gui.java
@@ -5,9 +5,31 @@ package theisland.gui;
  * @author Xavier
  */
 public class Gui {
+	/*
+	 * Display a string with a \n return at the end
+	 */
     public static void display(String displayString) {
         if (!displayString.isEmpty()) {
             System.out.println(displayString);
         }
+    }
+    
+    /*
+     * Display a string without a \n return at the end.
+     * Useful when information is asked to the user.
+     */
+    public static void displayInline(String displayString) {
+        if (!displayString.isEmpty()) {
+            System.out.print(displayString);
+        }
+    }
+    
+    /*
+     * Display an error with a \n return at the end
+     */
+    public static void displayError(String displayString) {
+    	if (!displayString.isEmpty()) {
+    		System.err.println(displayString);
+    	}
     }
 }
