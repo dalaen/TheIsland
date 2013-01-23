1cdff56c2f174f6a4a5911c862316e6ac0e12bb8
Xavier Poirot <xavier.poirot@dalaen.com>
2013-01-23 12:22:50 +0100

    Include save tests

--- src/theisland/TheIsland.java
+++ src/theisland/TheIsland.java
@@ -1,7 +1,10 @@
 package theisland;
 
+import theisland.castaway.Castaway;
 import theisland.gui.Gui;
+import theisland.savesystem.Save;
 import theisland.world.World;
+import theisland.world.exception.TooManyCastaway;
 
 /**
  *
@@ -15,9 +18,20 @@ public class TheIsland {
     public static void main(String[] args) {
         World.getInstance();
         
+        Save.getInstance().saveWorldData(World.getInstance());
+        
         Gui.display("There are "+ World.getInstance().getNumberOfCastaway() + " castaway on your island.");
         World.getInstance().printWeather();
         World.getInstance().nextDay();
         World.getInstance().printWeather();
+        
+        try {
+			World.getInstance().addCastaway(new Castaway("Hero"));
+		} catch (TooManyCastaway e) {
+			Gui.displayError("There is already too much castaway!");
+			e.printStackTrace();
+		}
+        Save.getInstance().saveCharacterData(World.getInstance().getCastaway(0));
+        Save.getInstance().saveCharacterData(World.getInstance().getCastaway(1));
     }
 }
