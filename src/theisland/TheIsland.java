6043351bcff9bf7b0f2085d2ab92cc2ae037be9a
Xavier Poirot <xavier.poirot@dalaen.com>
2013-01-30 12:02:55 +0100

    Adapt tests to the configuration file system

--- src/theisland/TheIsland.java
+++ src/theisland/TheIsland.java
@@ -2,6 +2,7 @@ package theisland;
 
 import theisland.castaway.Castaway;
 import theisland.gui.Gui;
+import theisland.item.food.ChickenLeg;
 import theisland.savesystem.Save;
 import theisland.world.World;
 import theisland.world.exception.TooManyCastaway;
@@ -16,7 +17,9 @@ public class TheIsland {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        World.getInstance();
+        World.getInstance().createWorld();
+        
+        //World.getInstance().getCastaway(0).getInventory().add(new ChickenLeg());
         
         Save.getInstance().saveWorldData(World.getInstance());
         
@@ -25,12 +28,12 @@ public class TheIsland {
         World.getInstance().nextDay();
         World.getInstance().printWeather();
         
-        try {
-			World.getInstance().addCastaway(new Castaway("Hero"));
-		} catch (TooManyCastaway e) {
-			Gui.displayError("There is already too much castaway!");
-			e.printStackTrace();
-		}
+//        try {
+//			World.getInstance().addCastaway(new Castaway("Hero"));
+//		} catch (TooManyCastaway e) {
+//			Gui.displayError("There is already too much castaway!");
+//			e.printStackTrace();
+//		}
         Save.getInstance().saveCharacterData(World.getInstance().getCastaway(0));
         Save.getInstance().saveCharacterData(World.getInstance().getCastaway(1));
     }
