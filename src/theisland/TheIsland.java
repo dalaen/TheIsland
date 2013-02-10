487ef4ef281b06cabe2a0c05e271800d222028ad
xavierpoirot <xavier.poirot@dalaen.com>
2013-02-10 21:35:14 +0100

    Add code for test purpose

--- src/theisland/TheIsland.java
+++ src/theisland/TheIsland.java
@@ -1,6 +1,12 @@
 package theisland;
 
+import java.util.ArrayList;
+
+import theisland.castaway.exception.InventoryOutOfRange;
 import theisland.gui.Gui;
+import theisland.item.Item;
+import theisland.item.food.ChickenLeg;
+import theisland.item.food.Mushroom;
 import theisland.savesystem.Save;
 import theisland.world.World;
 
@@ -17,6 +23,17 @@ public class TheIsland {
         World.getInstance().createWorld();
         
         Save.getInstance().saveWorldData(World.getInstance());
+        
+        // This is for test purpose
+        ArrayList<Item> inventory = new ArrayList<Item>();
+        inventory.add(new ChickenLeg());
+        inventory.add(new Mushroom());
+        try {
+			World.getInstance().getCastaway(0).setInventory(inventory);
+		} catch (InventoryOutOfRange e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         Gui.display("There are "+ World.getInstance().getNumberOfCastaway() + " castaway on your island.");
         World.getInstance().printWeather();
