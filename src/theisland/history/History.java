a5099fda58eb4580685677faa0f0ef2f035f3599
isen <isen@172.16.1.36>
2013-02-13 16:49:43 +0100

    Merge

--- src/theisland/history/History.java
+++ src/theisland/history/History.java
@@ -14,15 +14,17 @@ import theisland.gui.Gui;
  */
 public class History {
     
+	private final static int WAITING_TIME = 2000;
+	
     /**
      * Display the introduction of the game 
-     * @param mode Mode of the game : 1 for solo mode, 0 for multiplayer mode
+     * @param mode Mode of the game : SOLO, MULTIPLAYER
      */
-    public void displayIntroduction(int mode){
+    public static void displayIntroduction(Mode mode){
         Gui.display("On a beautiful summer day, the passengers of Flight 4125 from New York to Tahiti are serene, the flight is going well and dozens of beautiful islands are visible from the plane ...");
         
         try {
-            Thread.sleep(2000);
+            Thread.sleep(WAITING_TIME);
         } catch (InterruptedException ex) {
             Logger.getLogger(History.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -30,21 +32,21 @@ public class History {
         Gui.display("But suddenly, a terrible noise is heard, the plane began to shake, turn on itself and fall steeply to the sea ... Everyone starts screaming ...");
         
         try {
-            Thread.sleep(2000);
+            Thread.sleep(WAITING_TIME);
         } catch (InterruptedException ex) {
             Logger.getLogger(History.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        if(mode == 1) {
+        if(mode.equals(Mode.SOLO)) {
             Gui.display("Sunlight wake you up, you're lying on the beach, the carcass of the plane, decomposed, is a little farther down the beach.Bodies are floating on the water and nobody seems to have survived apart from you ... ");
         }
     
-        if(mode == 0) {
+        if(mode.equals(Mode.MULTIPLAYER)) {
             Gui.display("Sunlight wake you up, you're lying on the beach, the carcass of the plane, decomposed, is a little farther down the beach.Bodies are floating on the water, a person runs to you and asks if you're okay ... ");
         }
         
-        try {
-            Thread.sleep(2000);
+          try {
+            Thread.sleep(WAITING_TIME);
         } catch (InterruptedException ex) {
             Logger.getLogger(History.class.getName()).log(Level.SEVERE, null, ex);
         }
